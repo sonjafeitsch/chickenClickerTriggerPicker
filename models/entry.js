2fbc59a7f77cb9a29194b281ed6a12a4b5e31e35
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-02 19:42:14 +0100

    Define model for entry collection

--- models/entry.js
+++ models/entry.js
@@ -1,3 +1,17 @@
 /**
  * Created by sonja on 10/15/17.
  */
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var entrySchema = new Schema({
+    path: String,
+    name: String,
+    uploadDate: Date
+});
+
+var entryModel = mongoose.model('entryModel', entrySchema);
+
+module.exports(entryModel);
