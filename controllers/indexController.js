2a8e52b7c10bb0fe4c49bffb8d295bc9cdae3336
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-02 20:17:23 +0100

    Add basic routes for file upload

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,3 +5,7 @@
 exports.index = function(req, res) {
     res.render('index', { title: 'ChickenClickerTriggerPicker ', name: 'Chickens'});
 };
+
+exports.upload_file_post = function(req, res) {
+    res.send('file upload');
+};
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ var index_controller = require('../controllers/indexController');
 
 router.get('/', index_controller.index);
 
+router.post('/upload', index_controller.upload_file_post);
+
 module.exports = router;
