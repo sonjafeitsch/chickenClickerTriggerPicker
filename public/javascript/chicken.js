89ed44929724688a921a5d2af2135cb1044877a0
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-04 10:27:02 +0100

    Update jquery

--- public/javascript/chicken.js
+++ public/javascript/chicken.js
@@ -4,29 +4,30 @@
 
 function  clickEvent() {
     setTimeout(function(){
-        document.getElementById('clickBox').style.backgroundColor = '#e0e0e0';
+        $('#clickBox').css('backgroundColor', '#e0e0e0');
     }, 100);
 
-    var time = document.getElementById('chickenVideo').currentTime;
+    var time = $('#chickenVideo').get(0).currentTime;
     var correctTimes = [5, 17, 32, 80];
 
     for(var i=0;i<correctTimes.length;i++){
         if(Math.round(time)==correctTimes[i]){
-            document.getElementById('clickBox').style.backgroundColor = '#00ff00';
+            $('#clickBox').css('backgroundColor', '#00ff00');
             return;
         }
     }
-    document.getElementById('clickBox').style.backgroundColor = '#ff0000';
+    $('#clickBox').css('backgroundColor', '#ff0000');
     console.log(Math.round(time));
 }
 
 $(document).ready(function(){
-    var vid = document.getElementById('chickenVideo');
+    var vid = $('#chickenVideo');
+    var time = Math.round(vid.currentTime);
     vid.onplay = function() {
         var counter = 0;
         setInterval(function () {
             ++counter;
-            console.log("counter time: "+counter+"; video time: "+Math.round(document.getElementById('chickenVideo').currentTime));
+            console.log("counter time: "+counter+"; video time: "+time);
         }, 1000);
     };
     vid.onpause = function() {
