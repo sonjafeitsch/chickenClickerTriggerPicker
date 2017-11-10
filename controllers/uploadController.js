985aad484a0b0f7fa60034a85d33cfec36a3783c
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-10 08:59:58 +0100

    Add path to file in the template

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -7,10 +7,11 @@ exports.upload = function(req, res) {
         return res.status(400).send('No files were uploaded.');
 
     var file = req.files.file;
+    var path = __dirname + '/../uploads/' + file.name;
 
-    file.mv(__dirname + '/../uploads/' + file.name, function(err) {
+    file.mv(path, function(err) {
         if (err)
             return res.status(500).send(err);
-        res.render('upload', {title: 'ChickenClickerTriggerPicker ', name: 'Chickens', message: 'Successful upload!'});
+        res.render('upload', {title: 'ChickenClickerTriggerPicker ', name: 'Chickens', message: 'Successful upload!', pathToVideo: file.name});
     });
 };
