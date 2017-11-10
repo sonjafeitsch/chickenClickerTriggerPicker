e1f9034c842ba4165b2611a1e9bf575002a2bb96
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-11-10 08:59:07 +0100

    Make uploads folder public

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', index);
 
